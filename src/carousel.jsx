fd65b9af65c2e7d58cddd41ceb45a5c5ee058e05
Tom Lightfoot <42518772+tomlightfoot@users.noreply.github.com>
2019-04-29 11:13:09 +0100

    indicator functionality

--- src/carousel.jsx
+++ src/carousel.jsx
@@ -5,10 +5,12 @@ class Carousel extends Component {
   constructor(props) {
     super(props);
 
-    this.imgRefs = [];
+    this.state = {
+      index: 0
+    };
 
     this.carouselRef = React.createRef();
-    this.setImgRef = this.setImgRef.bind(this);
+    this.setActiveIndicator = this.setActiveIndicator.bind(this);
     this.scrollTo = this.scrollTo.bind(this);
     this.scroll = this.scroll.bind(this);
     this.offsetWidth = 0;
@@ -18,26 +20,35 @@ class Carousel extends Component {
     this.offsetWidth = this.props.children[0].ref.current.offsetWidth;
   }
 
-  setImgRef(img) {
-    this.imgRefs.push(img);
-  }
-
-  scrollTo(index) {
-    this.carouselRef.current.scrollLeft = parseInt(this.offsetWidth) * index;
-  }
-
   scroll(dir) {
     dir === "left"
       ? (this.carouselRef.current.scrollLeft -= this.offsetWidth)
       : (this.carouselRef.current.scrollLeft += this.offsetWidth);
   }
 
+  scrollTo(index) {
+    this.carouselRef.current.scrollLeft = parseInt(this.offsetWidth) * index;
+  }
+
+  setActiveIndicator() {
+    const index = this.carouselRef.current.scrollLeft / this.offsetWidth;
+    if (Number.isInteger(index)) {
+      this.setState({ index: index });
+    }
+  }
+
   render() {
+    const { children, buttons, indicators } = this.props;
+
     return (
       <div className="carouselContainer">
-        <div className="carousel mb-0" ref={this.carouselRef}>
-          {this.props.children}
-          {this.props.buttons && (
+        <div
+          className="carousel mb-0"
+          ref={this.carouselRef}
+          onScroll={this.setActiveIndicator}
+        >
+          {children}
+          {buttons && (
             <React.Fragment>
               <button className="left" onClick={() => this.scroll("left")}>
                 Left
@@ -48,10 +59,18 @@ class Carousel extends Component {
             </React.Fragment>
           )}
 
-          {this.props.indicators && (
+          {indicators && (
             <ol className="carousel-indicators">
-              {this.props.children.map((img, index) => {
-                return <li onClick={() => this.scrollTo(index)} />;
+              {children.map((img, index) => {
+                return (
+                  <li
+                    className={
+                      "carousel-indicator " +
+                      (this.state.index === index ? "active" : "")
+                    }
+                    onClick={() => this.scrollTo(index)}
+                  />
+                );
               })}
             </ol>
           )}
