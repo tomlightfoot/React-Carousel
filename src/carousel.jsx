b815d0444b04c0f1837daea50ce4480b09becadb
Tom Lightfoot <42518772+tomlightfoot@users.noreply.github.com>
2019-05-08 12:43:18 +0100

    working

--- src/carousel.jsx
+++ src/carousel.jsx
@@ -46,9 +46,9 @@ class Carousel extends Component {
       this.carouselRef.current.scrollLeft /
       this.props.children[0].ref.current.offsetWidth;
     this.state.index < Math.round(scrollPosition) &&
-      this.setState({ translate: this.state.translate - 14 });
+      this.setState({ translate: this.state.translate - 16 });
     this.state.index > Math.round(scrollPosition) &&
-      this.setState({ translate: this.state.translate + 14 });
+      this.setState({ translate: this.state.translate + 16 });
 
     this.setState({
       index: Math.round(scrollPosition)
@@ -56,7 +56,7 @@ class Carousel extends Component {
   }
 
   render() {
-    const { children, buttons, indicators } = this.props;
+    const { children, counter, buttons, indicators } = this.props;
 
     return (
       <div className="carouselContainer">
@@ -66,52 +66,81 @@ class Carousel extends Component {
           onScroll={this.setActiveIndicator}
         >
           {children}
-          {buttons && (
-            <React.Fragment>
-              <button className="left" onClick={() => this.scroll("left")}>
-                Left
-              </button>
-              <button className="right" onClick={() => this.scroll("right")}>
-                Right
-              </button>
-            </React.Fragment>
-          )}
-          {indicators && (
-            <ol
-              className="carousel-indicators"
-              style={{
-                transform: "translateX(" + this.state.translate + "px)",
-                transition: "0.6s"
-              }}
-            >
-              {children.map((img, index) => {
-                return (
-                  <li
-                    className={
-                      "carousel-indicator " +
-                      (this.state.index === index ? "active " : "") +
-                      (Math.abs(this.state.index - index) === 1
-                        ? "large"
-                        : "") +
-                      (Math.abs(this.state.index - index) === 2
-                        ? "medium"
-                        : "") +
-                      (Math.abs(this.state.index - index) === 3
-                        ? "small"
-                        : "") +
-                      (Math.abs(this.state.index - index) === 4
-                        ? "ex-small"
-                        : "") +
-                      (Math.abs(this.state.index - index) > 4 ? "inactive" : "")
-                    }
-                    key={index}
-                    onClick={() => this.scrollTo(index)}
-                  />
-                );
-              })}
-            </ol>
-          )}
         </div>
+        {buttons && (
+          <React.Fragment>
+            <label className="left" onClick={() => this.scroll("left")}>
+              <svg
+                aria-hidden="true"
+                role="presentation"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+              </svg>
+              <span className="sr-only">Previous</span>
+            </label>
+            <label className="right" onClick={() => this.scroll("right")}>
+              <svg
+                aria-hidden="true"
+                role="presentation"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+              </svg>
+              <span className="sr-only">Next</span>
+            </label>
+          </React.Fragment>
+        )}
+        {indicators && (
+          <ol
+            className="indicators"
+            style={{
+              transform: "translateX(" + this.state.translate + "px)",
+              transition: "0.6s"
+            }}
+          >
+            {children.map((img, index) => {
+              return (
+                <li
+                  className={
+                    "indicator " +
+                    (this.state.index === index ? "active-ind " : "") +
+                    (Math.abs(this.state.index - index) === 1
+                      ? "large-ind"
+                      : "") +
+                    (Math.abs(this.state.index - index) === 2
+                      ? "medium-ind"
+                      : "") +
+                    (Math.abs(this.state.index - index) === 3
+                      ? "small-ind"
+                      : "") +
+                    (Math.abs(this.state.index - index) === 4
+                      ? "ex-small-ind"
+                      : "") +
+                    (Math.abs(this.state.index - index) > 4
+                      ? "inactive-ind"
+                      : "")
+                  }
+                  key={index}
+                  onClick={() => this.scrollTo(index)}
+                />
+              );
+            })}
+          </ol>
+        )}
+        {counter && (
+          <div className="mt-2 test">
+            {this.state.index + 1}/{children.length}
+          </div>
+        )}
       </div>
     );
   }
